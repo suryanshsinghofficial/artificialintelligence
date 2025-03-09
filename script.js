ff380971af18d2a5b9079a4ae27b2eba4a8ffaf7
Suryansh Singh <suryanshsinghrajput.business@gmail.com>
2025-03-09 10:14:05 +0000

    “//Comment”

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function alertMessage() {
-    alert("You clicked the button!");
-}
+function toggleNav() {
+    const nav = document.querySelector('nav ul');
+    nav.classList.toggle('nav-active');
+  }
+  
+  function scrollToSection(id) {
+    const section = document.getElementById(id);
+    window.scrollTo({
+      top: section.offsetTop,
+      behavior: 'smooth'
+    });
+  }
+  
